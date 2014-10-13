f1467e6a72cdadfe3c1770d9de9d6794830a7f69
Ben Diuguid <Bendiuguid@gmail.com>
2014-10-13 18:35:23 -0400

    tweaking node-intro

---
+++ sub-pages/node-intro/HelloWorld.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  console.log("request received.");
+  response.end("<h1>Hello World</h1>");
+});
+
+server.listen(8000);
+
+console.log("Server running at http://127.0.0.1:8000/");
