af32e3d46c13e329108b2ad7c9619719e31167de
Ben Diuguid <Bendiuguid@gmail.com>
2014-10-14 21:34:49 -0400

    tweaked node-intro

--- sub-pages/node-intro/demo/HelloWorld.js
+++ sub-pages/node-intro/demo/HelloWorld.js
@@ -9,3 +9,6 @@ var server = http.createServer(function (request, response) {
 server.listen(8000);
 
 console.log("Server running at http://127.0.0.1:8000/");
+
+// closure example
+//var init = function () {var numbers = [ 1, 2, 4, 8, 16, 32 ];return {addEntry: function () {numbers.push( Math.pow(2, numbers.length) );},get: function (n) {if( n < numbers.length ) {return numbers[n];} else {console.err("Nice try!");}}};};
