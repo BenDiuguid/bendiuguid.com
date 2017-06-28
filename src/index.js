fe65975b126daaa5e690d578ed8852efb760bb92
Ben Diuguid <bendiuguid@gmail.com>
2017-06-27 21:15:38 -0700

    dynamically load css

--- src/index.js
+++ src/index.js
@@ -6,7 +6,20 @@ const main = () => {
   // - `./analytics/multiple-trackers.js`
   import('./analytics/base.js').then(analytics => analytics.init());
 
+  // Dynamically load css
+  dynamicLoadStylesheet('https://fonts.googleapis.com/css?family=Ubuntu');
+  dynamicLoadStylesheet(
+    'https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css'
+  );
+
   // Initate all other code paths here...
+};
+
+const dynamicLoadStylesheet = url => {
+  const stylesheet = document.createElement('link');
+  stylesheet.href = url;
+  stylesheet.rel = 'stylesheet';
+  document.getElementsByTagName('head')[0].appendChild(stylesheet);
 };
 
 // Start the app through its main entry point.
